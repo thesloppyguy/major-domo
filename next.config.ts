cf7bb3720b28ae31c917dd3b741282e9e910e217
thesloppyguy <sahilmandal@outlook.com>
2025-05-14 05:00:20 +0530

    Fix: Next config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["images.unsplash.com", "www.ignant.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "www.ignant.com",
+      },
+    ],
   },
   // async redirects() {
   //   return [
